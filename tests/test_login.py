ff0318687439d9911fd1728f448a47f8477ac5de
celina2123 <celinaelheddad@gmail.com>
2026-02-04 12:05:23 +0100

    Implement login success test with Selenium
    Added a test for successful login using Selenium.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+def test_login_success():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    time.sleep(2)
+
+    assert "inventory.html" in driver.current_url
+
+    driver.quit()
 
